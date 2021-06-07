8c519de6ca795000b492a43913777892794c5c71
jinook929 <jinook929@gmail.com>
2021-06-06 22:43:47 -0400

    finish reversestring

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -7,7 +7,32 @@
 //   reverse('Greetings!') === '!sgniteerG'
 
 function reverse(str) {
+  // return str.split("").reverse().join("");
+
+  // let result = []
+  // for(let i = 0; i < str.length; i++) {
+  //   result.unshift(str[i])
+  // }
+  // return result.join("")
   
+  // let result = "";
+  // for(let i = str.length - 1; i >= 0; i--) {
+  //     result += str[i];
+  // }
+  // return result;
+
+  // let reversed = ""
+  // for(const ch of str) {
+  //   reversed = ch + reversed
+  // }
+  // return reversed
+
+  // return str.split("").reduce((acc, el) => el + acc, "")
+  
+  // if(str === "") return ""
+  // return reverse(str.substring(1)) + str[0]
+
+  return str === "" ? "" : reverse(str.substring(1)) + str[0]
 }
 
 module.exports = reverse;
